f34ca275dc2debc166f7fa0f6c82eacc742f06a5
RolandMarton <roland111187@gmail.com>
2025-08-27 20:52:06 +0200

    Add colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold font-zentry">Welcome to Awwaards</h1>
+      <h1 className="text-5xl font-bold text-violet-300">Welcome to Awwaards</h1>
     </main>
   );
 };
