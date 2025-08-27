23520ceaa4e655f9484ed2cd5187a2e528abbe15
RolandMarton <roland111187@gmail.com>
2025-08-28 01:21:36 +0200

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl font-bold text-violet-300">Welcome to Awwaards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <div style={{ height: 500}}></div>
     </main>
   );
 };
